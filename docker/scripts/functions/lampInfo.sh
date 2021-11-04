f7368c61805252668cee580e40ad560a6fafced2
Ruben Siota Perez <rsiota.gmail.com>
2021-11-04 23:03:02 +0000

    Add lamp restart command

--- docker/scripts/functions/lampInfo.sh
+++ docker/scripts/functions/lampInfo.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+source "${SCRIPTS_PATH}/functions/checkRunningPhpContainer.sh"
+source "${SCRIPTS_PATH}/functions/checkRunningMysqlContainer.sh"
+
 function lampInfo {
-    echo "you typed info"
+
+    PHP_CONTAINER=$(checkRunningPhpContainer)
+    PHP_VERSION=${PHP_CONTAINER:3:2}
+    printf "PHP version: $PHP_VERSION\n"
+    MYSQL_CONTAINER=$(checkRunningMysqlContainer)
+    printf "MySQL version: 5.6"
 }
 
 export -f lampInfo
